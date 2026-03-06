dbe83406fece802151a6e6ef5819a76dd613a53f
Mohamed <mohamedafifsmaili@gmail.com>
2026-03-05 22:21:06 -0300

    style: adicao de cores e fonte

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.titulo}>Open up ppo start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#814141',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo: {
+    fontSize:50,
+    color: '#ffffff',
+    fontWeight: 'bold'
+  }
+
 });
