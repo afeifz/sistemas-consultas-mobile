18b811d0b342a40594f94879ff491f983de9b6c7
Mohamed <mohamedafifsmaili@gmail.com>
2026-05-07 21:39:55 -0300

    feat criacao do componente de botao

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,7 @@
-export { default as ConsultaCard } from "./consultaCard";
-// mudança no texto
+/**
+ * Exportação centralizada dos componentes
+ */
 
+export { default as ConsultaCard } from "./ConsultaCard";
+export { default as Loading } from "./Loading";
+export { default as EmptyState } from "./EmptyState";
