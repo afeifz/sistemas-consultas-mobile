87c8e56dd2caa56b244ea6bbf70c910a22cd49fd
Mohamed <mohamedafifsmaili@gmail.com>
2026-08-13 21:53:31 -0300

    feat: nivelamento

--- src/interfaces/consulta.ts
+++ src/interfaces/consulta.ts
@@ -1,20 +1,16 @@
-import { Especialidade } from "../types/especialidade";
-import { StatusConsulta } from "../types/statusConsulta";
-
-
-
+import { StatusConsulta, Especialidade } from "../types";
 
 export interface Consulta {
- id: number;
- pacienteId: number;
- pacienteNome: string;
- medicoId: number;
- medicoNome: string;
- especialidade: Especialidade;
- usuarioId: number; // ID do usuário que agendou a consulta
- data: string;
- horario: string;
- status: StatusConsulta;
- observacoes?: string;
- valor?: number;
+  id: number;
+  pacienteId: number;
+  pacienteNome: string;
+  medicoId: number;
+  medicoNome: string;
+  especialidade: Especialidade;
+  usuarioId: number; // ID do usuário que agendou a consulta
+  data: string;
+  horario: string;
+  status: StatusConsulta;
+  observacoes?: string;
+  valor?: number;
 }
